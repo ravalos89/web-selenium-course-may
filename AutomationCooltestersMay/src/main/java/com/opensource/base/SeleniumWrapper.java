d36c83786b7ba4b1108c477d80271129852c61d6
ravalos89 <automationricardo1409@gmail.com>
2021-07-08 20:10:57 -0500

    rrrrr

--- AutomationCooltestersMay/src/main/java/com/opensource/base/SeleniumWrapper.java
+++ AutomationCooltestersMay/src/main/java/com/opensource/base/SeleniumWrapper.java
@@ -262,5 +262,14 @@ public class SeleniumWrapper {
 			return null;
 		}
 	}
+	
+	private void sleep(int seg) {
+		try {
+			Thread.sleep(seg*1000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
