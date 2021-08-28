35efed1c1d2fd9b99d542419eb5bab4d1b8abf7c
ravalos89 <automationricardo1409@gmail.com>
2021-08-28 09:11:06 -0500

    new dev

---
+++ AutomationCooltestersMay/src/test/java/com/opensource/admin/qa/TC008_Admin_Login.java
@@ -0,0 +1,35 @@
+package com.opensource.admin.qa;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+
+public class TC008_Admin_Login {
+  @Test
+  public void TC008_Admin_Login_Script() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver2.exe");
+		WebDriver driver = new ChromeDriver();
+
+		// Step 1 - Open Browser "OrangeHRM" web page
+		Reporter.log("Open Browser \"OrangeHRM\" web page");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.close();
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
